d75a9c6fe9b07e3c5481cde8c467f5635d769506
Andrei Gatej <andreigtj01@gmail.com>
2023-09-21 22:08:55 +0300

    feat(api): add global authentication guard

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,6 +6,9 @@ import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
 import { UserModule } from './entities/user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './entities/post/post.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthenticateGuard } from './guards/authenticate.guard';
 
 @Module({
   imports: [
@@ -15,11 +18,16 @@ import { AuthModule } from './auth/auth.module';
     DbModule,
     UserModule,
     AuthModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [
     AppService,
     SESSION_MIDDLEWARE_PROVIDER,
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticateGuard,
+    },
   ],
 })
 export class AppModule {}
