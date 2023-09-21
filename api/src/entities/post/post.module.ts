4353a9aed26594a2e0f8260fe333639a723cb354
Andrei Gatej <andreigtj01@gmail.com>
2023-09-21 22:54:42 +0300

    feat(post): configure multer for file upload

--- api/src/entities/post/post.module.ts
+++ api/src/entities/post/post.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { MulterModule } from '@nestjs/platform-express';
+import * as path from 'path'
+import * as multer from 'multer';
+import * as crypto from 'crypto'
+
+const storage = multer.diskStorage({
+  destination: path.join(process.cwd(), 'post_files'),
+  filename(req, file, callback) {
+    const randomChars = crypto.randomBytes(18).toString('hex');
+    const fileName = `${randomChars}${path.extname(file.originalname)}`
+
+    callback(null, fileName);
+  },
+})
 
 @Module({
   controllers: [PostController],
-  providers: [PostService]
+  providers: [PostService],
+  imports: [
+    MulterModule.register({
+      storage,
+    })
+  ]
 })
-export class PostModule {}
+export class PostModule { }
