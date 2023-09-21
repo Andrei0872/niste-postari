dff42bff353e2b8ad189b91c7ad043c3e1184cb1
Andrei Gatej <andreigtj01@gmail.com>
2023-09-22 00:12:20 +0300

    feat(api): configure cors for accepting only one client

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SESSION_MIDDLEWARE_TOKEN } from './middlewares/session.middleware';
+import { config } from './config';
+import * as cors from 'cors';
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,6 +18,12 @@ async function bootstrap() {
 
   const sessionMiddleware = app.get(SESSION_MIDDLEWARE_TOKEN);
   app.use(sessionMiddleware);
+
+  app.use(cors({
+    origin: config.CLIENT_URL,
+    credentials: true,
+  }));
+
 
   await app.listen(PORT);
 }
