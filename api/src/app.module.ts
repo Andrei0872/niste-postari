bc164d66b31029477653257aa6b32b5c0cf767ce
Andrei Gatej <andreigtj01@gmail.com>
2023-09-21 20:09:46 +0300

    feat(api): connect to the db

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SESSION_MIDDLEWARE_PROVIDER } from './middlewares/session.middleware';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env'
-    })
+    }),
+    DbModule,
   ],
   controllers: [AppController],
   providers: [
