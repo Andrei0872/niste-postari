34b9c13d66265f66eada40e287dcba6b724f7624
Andrei Gatej <andreigtj01@gmail.com>
2023-09-21 22:19:07 +0300

    chore(api): seed with a bigger number of posts

--- api/data/seeds/02_posts.ts
+++ api/data/seeds/02_posts.ts
@@ -6,31 +6,13 @@ export async function seed(knex: Knex) {
   await knex("post").del();
 
   const userIds = (await knex("user").select('id')).map(r => r.id);
-  
-  await knex("post").insert([
-    {
-      title: 'title#1',
+
+  const posts = Array.from({ length: 60 })
+    .map((_, idx) => ({
+      title: `title#${idx + 1}`,
       body: samplePostBody,
       image_url: path.join(process.cwd(), 'post_files', 'golden.jpg'),
-      poster_id: userIds[0],
-    },
-    {
-      title: 'title#2',
-      body: samplePostBody,
-      image_url: path.join(process.cwd(), 'post_files', 'golden.jpg'),
-      poster_id: userIds[0],
-    },
-    {
-      title: 'title#3',
-      body: samplePostBody,
-      image_url: path.join(process.cwd(), 'post_files', 'golden.jpg'),
-      poster_id: userIds[1],
-    },
-    {
-      title: 'title#4',
-      body: samplePostBody,
-      image_url: path.join(process.cwd(), 'post_files', 'golden.jpg'),
-      poster_id: userIds[1],
-    },
-  ]);
+      poster_id: userIds[idx % 2],
+    }));
+  await knex("post").insert(posts);
 }
